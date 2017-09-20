f543b0f879ad82006dea694b2fdfd38786a428fe
Victoria Rudakova <vicrucann@protonmail.com>
2017-09-20 09:46:28 -0400

    type_name edited to more verbose version

--- 004-typeDeduction/types.cpp
+++ 004-typeDeduction/types.cpp
@@ -3,24 +3,46 @@
 #include <string>
 #include <typeinfo>
 #include <memory>
+#include <experimental/string_view>
 
 // type_name() is taken from https://stackoverflow.com/a/20170989/4118460
+// C++17 version:
 template <class T>
-std::string type_name()
+constexpr std::experimental::string_view type_name()
 {
-    typedef typename std::remove_reference<T>::type TR;
-    std::unique_ptr<char, void(*)(void*)> own(nullptr, std::free);
-    std::string r = own != nullptr ? own.get() : typeid(TR).name();
-    if (std::is_const<TR>::value)
-        r += " const";
-    if (std::is_volatile<TR>::value)
-        r += " volatile";
-    if (std::is_lvalue_reference<T>::value)
-        r += "&";
-    else if (std::is_rvalue_reference<T>::value)
-        r += "&&";
-    return r;
+    using namespace std::experimental;
+#ifdef __clang__
+    string_view p = __PRETTY_FUNCTION__;
+    return string_view(p.data() + 34, p.size() - 34 - 1);
+#elif defined(__GNUC__)
+    string_view p = __PRETTY_FUNCTION__;
+#  if __cplusplus < 201402
+    return string_view(p.data() + 36, p.size() - 36 - 1);
+#  else
+    return string_view(p.data() + 46, p.size() - 46 - 1);
+#  endif
+#elif defined(_MSC_VER)
+    string_view p = __FUNCSIG__;
+    return string_view(p.data() + 38, p.size() - 38 - 7);
+#endif
 }
+// C++11 version:
+//template <class T>
+//std::string type_name()
+//{
+//    typedef typename std::remove_reference<T>::type TR;
+//    std::unique_ptr<char, void(*)(void*)> own(nullptr, std::free);
+//    std::string r = own != nullptr ? own.get() : typeid(TR).name();
+//    if (std::is_const<TR>::value)
+//        r += " const";
+//    if (std::is_volatile<TR>::value)
+//        r += " volatile";
+//    if (std::is_lvalue_reference<T>::value)
+//        r += "&";
+//    else if (std::is_rvalue_reference<T>::value)
+//        r += "&&";
+//    return r;
+//}
 
 int foo(int x, int y)
 {
